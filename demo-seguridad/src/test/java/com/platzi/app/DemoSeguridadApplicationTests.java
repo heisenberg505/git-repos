0755faab8048df920c349d5f9f0ee4422c2c276c
heisenberg505 <fate.fabian@gmail.com>
2025-01-28 11:39:23 -0600

    Nuevo demo para seguridad git

---
+++ demo-seguridad/src/test/java/com/platzi/app/DemoSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
